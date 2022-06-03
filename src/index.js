d9b20fd997745c40df05b1075459b55c6985b5b8
mrjohnming <john.ngai@hotmail.com>
2022-06-03 14:26:15 -0400

    Render ReactDOM

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './style.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
