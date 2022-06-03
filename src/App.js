b2b701ddbee43bb5af342f4349b877ea01422394
mrjohnming <john.ngai@hotmail.com>
2022-06-03 15:59:16 -0400

    Create function - allNewDice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import Die from "./components/Die";
 
+// Return an array of 10 random numbers between 1-6 inclusive.
+const allNewDice = () => {
+  const newDice = [];
+  for (let count = 1; count <= 10; count++) {
+    newDice.push(Math.ceil(Math.random() * 6));
+  }
+  return newDice;
+}
+
 export default function App() {
   return (
     <main>
