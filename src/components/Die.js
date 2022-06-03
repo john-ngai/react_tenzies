81009147516ff9e8b0d0dd8baed2f4f6afa281d5
mrjohnming <john.ngai@hotmail.com>
2022-06-03 17:16:14 -0400

    Refactor Die component background-color styling from stylesheet to inline

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,10 @@
 export default function Die(props) {
-  return (
-    <div className={props.isHeld ? "die--face hold" : "die--face"}>
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#FFF"
+  };
+
+  return ( 
+    <div className="die--face" style={styles}>
       <h2 className="die--num">{props.value}</h2>
     </div>
   );
